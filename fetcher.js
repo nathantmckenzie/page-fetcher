af89a314f5fd1ffe360649cfcaea9e9a9e91750f
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-05 08:25:27 -0800

    Website fetcher app

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,11 @@
 const request = require('request');
 const fs = require('fs');
-request(process.argv[2], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
-
-fs.writeFile(process.argv[3], 'Page Downloader', function(err) {
+const http = require('http');
+request(process.argv[2], () => {
+  fs.writeFile(process.argv[3], process.argv[2], function(err) {
     if (err) throw err;
     console.log('Downloaded and saved 3261 bytes to ./index.html');
   }); 
+});
+
+
